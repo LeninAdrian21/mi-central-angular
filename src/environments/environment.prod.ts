42463ae8e479769c1da51e3ed2ac726b8f475562
LeninAdrian21 <leni.adrian08@outlook.com>
2023-05-10 14:57:14 -0600

    Se modifico las variables de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: "http://localhost:1337",
+  API_URL: "https://micentral-api.onrender.com",
   useValue: "6Ldi6mgkAAAAAPyFgTgeIli5r_dDNK_Iu8Hu_CYA",
   Administrador: '633cc31197ead65f9859ab6f',
   Delivery_man: '633cc33497ead65f9859ab70',
