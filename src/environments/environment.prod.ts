ead4501f0e70b53aac29f251f986faef5d82a246
LeninAdrian21 <leni.adrian08@outlook.com>
2023-05-10 13:42:48 -0600

    Se pusieron las variables de entorno en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: "http://localhost:1337",
+  useValue: "6Ldi6mgkAAAAAPyFgTgeIli5r_dDNK_Iu8Hu_CYA",
+  Administrador: '633cc31197ead65f9859ab6f',
+  Delivery_man: '633cc33497ead65f9859ab70',
+  Manager: '633cc35097ead65f9859ab71',
 };
