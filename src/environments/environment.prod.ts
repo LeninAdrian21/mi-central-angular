4d3b9f1c77ae02b2c7a86ec9bfdd47e1aca0e9bf
LeninAdrian21 <leni.adrian08@outlook.com>
2023-07-06 02:46:42 -0600

    Se agrego una variable en los envioroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   Administrador: '633cc31197ead65f9859ab6f',
   Delivery_man: '633cc33497ead65f9859ab70',
   Manager: '633cc35097ead65f9859ab71',
+  User:'64a32140e36daf2da8ca7570',
   recaptcha: {
     siteKey: '6LeDZVkkAAAAAAieZFHU4io4Qec9n2BPOBB-Jt3c',
   },
